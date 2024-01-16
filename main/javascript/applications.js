49521f9c6e8b65c1cc002ec988549474978b1b5c
ouhoy <kimkinon@gmail.com>
2024-01-16 17:22:50 +0100

    Add applications dashboard

---
+++ main/javascript/applications.js
@@ -0,0 +1,38 @@
+import {collection, db, doc, getDocs} from "./dataBase.js";
+
+const usersRef = collection(db,"applications");
+
+
+const table = document.getElementById("table");
+getDocs(usersRef).then(querySnapshot => {
+    querySnapshot.forEach(userDoc => {
+        let docRef;
+        const application = userDoc.data();
+        const HTML  = `            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" style="text-transform: capitalize;" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    ${application.fullName}
+                </th>
+                <td style="text-transform: capitalize;" class="px-6 py-4">
+                    ${application.title}
+                </td>
+
+                <td style="text-transform: capitalize;" class="px-6 py-4">
+                    <a id="${userDoc.id}" href="#" class="text-sm font-medium text-indigo-600">View Application</a>
+                </td>
+
+                <td style="text-transform: capitalize;" class="px-6 py-4">
+                   No State
+                </td>
+            </tr>
+
+
+`
+        table.insertAdjacentHTML("beforeend", HTML);
+
+
+    });
+
+})
+
+
+
